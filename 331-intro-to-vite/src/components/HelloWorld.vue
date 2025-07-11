07558ab2a2d5cb2f81a952eba7b513ee297653fe
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-11 17:10:33 +0700

    finish 3.2

--- 331-intro-to-vite/src/components/HelloWorld.vue
+++ 331-intro-to-vite/src/components/HelloWorld.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
-defineProps<{
-  msg: string
-}>()
+// defineProps<{
+//   msg: string
+// }>()
 </script>
 
 <template>
-  <div class="greetings">
+  <!-- <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
-  </div>
+  </div> -->
+  <div class="greetings"></div>
 </template>
 
-<style scoped>
+<!-- <style scoped>
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
@@ -38,4 +39,4 @@ h3 {
     text-align: left;
   }
 }
-</style>
+</style> -->
