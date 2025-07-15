9348369d74106f420093674539a0976ae3253d6c
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-15 19:50:38 +0700

    finish 8.7

--- 331-intro-to-vite/src/views/HomeView.vue
+++ 331-intro-to-vite/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
-import type { Event } from '@/type'
+import CategoryCard from '@/components/CategoryCard.vue'
+import type { Event } from '@/types'
 import { ref } from 'vue'
 const events = ref<Event[]>([
   {
@@ -40,7 +41,16 @@ const events = ref<Event[]>([
 </script>
 
 <template>
-  <div class="home">
+  <div class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <CategoryCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
