475e842638c2a2e75c6b2ee601a9f669f87a7e0c
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-17 16:48:59 +0700

    finish 1.4

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
 import InfoView from '@/views/InfoView.vue'
+import EventDetailView from '@/views/EventDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,6 +24,11 @@ const router = createRouter({
       path: '/info',
       name: 'info',
       component: InfoView
+    },
+    {
+      path: '/event/5928101',
+      name: 'event-detail-view',
+      component: EventDetailView
     }
   ],
 })
