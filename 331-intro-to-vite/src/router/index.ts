f565c04779eb7ff8c8ec7dc186727a8d264e2a60
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-18 09:31:34 +0700

    finish 3.3

--- 331-intro-to-vite/src/router/index.ts
+++ 331-intro-to-vite/src/router/index.ts
@@ -2,7 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
 import InfoView from '@/views/InfoView.vue'
-import EventDetailView from '@/views/EventDetailView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +15,32 @@ const router = createRouter({
       name: 'event-list-view',
       component: EventListView,
       props: (route) => ({ page: parseInt(route.query.page?.toString() || '1') })
+    },
+    {
+      path: '/event/:id',
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
     },
     {
       path: '/about',
@@ -25,14 +54,8 @@ const router = createRouter({
       path: '/info',
       name: 'info',
       component: InfoView
-    },  
-    {
-      path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
-      props: true
     }
-  ],
+  ]
 })
 
 export default router
---
+++ 331-intro-to-vite/src/views/event/EditView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { type Event} from '@/types'
+
+const props = defineProps<{
+    event: Event,
+    id: String
+}> ()
+
+// eslint-disable-next-line @typescrpit-eslint/no-unused-vars
+
+const { event } = toRefs(props)
+</script>
+
+<template>
+    <p>Edit event here</p>
+</template>
