cc0fa972278839e0eb2d90fcfae0433f0e289c59
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-18 09:50:06 +0700

    finish 5.2

---
+++ 331-intro-to-vite/src/views/NetworkErrorView.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="networkError">
+        <h1>Un-Oh!</h1>
+        <h3>
+            It looks like you're experiencing some network issue, please take a breathe and 
+            <a href="#" @click="$router.go(-1)">click here</a> to try again.
+        </h3>
+    </div>
+</template>
