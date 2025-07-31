4e189ebba21dc14f239d11947cc15077b487b77d
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-07-31 22:41:49 +0700

    finish lab 05 5

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -26,7 +26,7 @@ defineProps<{
     </h3>
   </div> -->
   <RouterLink :to="{ name: 'event-detail-view', params: { id: event.id }}">
-    <div class="event-card">
+    <div class="cursor-pointer border border-gray-600 p-4 w-64 mb-6 hover:scale-101 hover:shadow-sp">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
     </div>
