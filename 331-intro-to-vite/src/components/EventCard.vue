574d69af2d5e9880c7661c34d2dac67e130a2542
saw_rory_yin <sawroryyinmky.26@gmail.com>
2025-08-01 09:27:03 +0700

    finish lab 05 13.6 delete all css

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -32,17 +32,3 @@ defineProps<{
     </div>
   </RouterLink>
 </template>
-
-<style scoped>
-.event-card{
-  padding: 20px;
-  width: 250px;
-  cursor: pointer;
-  border: 1px solid #39495c;
-  margin-bottom: 18px;
-}
-.event-card:hover{
-  transform: scale(1.01);
-  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2)
-}
-</style>
